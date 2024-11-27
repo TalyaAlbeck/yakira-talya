dd9e12e7061eac3056d1a79773d0baf95731a4c3
yakirahordes <yakirahordes@hilma.tech>
2024-11-27 13:24:21 +0200

    login

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { postRequest } from "../functions/postRequest";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  //   useEffect(() => {
+  //     (async () => await fetchData(`users`, "data", setUsersData, setError))();
+  //   }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const currentUser = postRequest({ username, password });
+    if (currentUser) {
+      alert("success");
+      //   navigate(`/home/${currentUser.id}`);
+    } else {
+      setError("this user does not exist");
+    }
+  }
+
+  //   function checkUserData() {
+  //     const currentUser = usersData.filter(
+  //       (user) => user.username === username && user.website === password
+  //     )[0];
+  //     localStorage.setItem(
+  //       "currentUserId",
+  //       JSON.stringify(currentUser?.id) || null
+  //     );
+  //     return currentUser;
+  //   }
+
+  useEffect(() => {
+    window.history.pushState(null, "", window.location.href);
+    window.onpopstate = function () {
+      window.history.pushState(null, "", window.location.href);
+    };
+  }, []);
+
+  return (
+    <form className="login-form" onSubmit={handleSubmit}>
+      <h1 className="inside-form">Login</h1>
+      <label className="inside-form">Username:</label>
+      <input
+        className="inside-form"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <br />
+      <label className="inside-form">Password:</label>
+      <input
+        className="inside-form"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+      <button className="inside-form">Submit</button>
+      <br />
+      <Link className="inside-form" to="/register">
+        Sign Up
+      </Link>
+      <p className="inside-form">{error}</p>
+    </form>
+  );
+}
--- server/routes/create.js
+++ server/routes/create.js
@@ -1,14 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { addItem } = require('./../SQL/addItem')
+const { addItem } = require("./../SQL/addItem");
 
-
-router.post('/user', function (req, res) {
-    console.log("start");
-
-    addItem("user", "username, email, phone", { username: JSON.stringify(req.body.username), email: JSON.stringify(req.body.email), phone: JSON.stringify(req.body.phone) })
-    // addItem("user", "username, email, phone", `"talya", "talya@nini.com", "0520000000"`)
-    res.send(res);
+router.post("/user", function (req, res) {
+  const result = addItem("user", "username, email, phone", {
+    username: JSON.stringify(req.body.username),
+    email: JSON.stringify(req.body.email),
+    phone: JSON.stringify(req.body.phone),
+  });
+  if (err) {
+    res = err;
+  } else {
+    res = "user added";
+  }
+  res.send("user added");
 });
 
 module.exports = router;
